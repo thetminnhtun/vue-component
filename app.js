1cfd37f3bf68a571d4e4ca5a4af994ee17748160
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-06-06 22:03:45 +0630

    Props

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 Vue.component('post', {
 	template: `
 	<div>
-		<h1><slot name="title">Post Title</slot></h1>
-		<p><slot name="content">Post Content</slot></p>
+		<h1>{{ name }}</h1>
+		<p>{{ content }}</p>
 	</div>
-	`
+	`,
+	props: {
+		name: {
+			type: String,
+			default: 'This is default title'
+		},
+		content: String
+	}
 })
 
 new Vue({
-	el: '#app'
+	el: '#app',
+	data: {
+		posts: [
+			{title: 'Post One', content: 'Post one content'},
+			{title: 'Post Two', content: 'Post two content'},
+			{title: 'Post Three', content: 'Post three content'},
+		]
+	}
 })
